b1baa7bc2d1e98e44b3e967731cc8a14a3cf7b39
anzhub101 <anzeemug@gmail.com>
2022-02-22 14:52:51 +0100

    update on work

--- utils/card.py
+++ utils/card.py
@@ -1,8 +1,19 @@
 class Symbol:
-    def __init__(self, color):
-        self.color=color
-        self.icon=['♥','♦', '♣', '♠']
+    def __init__(self, icon):
+        if icon in ['♥', '♦', '♣', '♠']:
+            self.icon = icon
+        else:
+            print('Invalid symbol')
+
+        if self.icon == '♥' or self.icon == '♦':
+            self.color = 'red'
+        else:
+            self.color = 'black'
+
 
 class Card(Symbol):
     def __int__(self, value):
-        self.value=['A', '2', '3', '4 ', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        if value in ['A', '2', '3', '4 ', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
+            self.value=value
+        else:
+            print('invalid value')
--- utils/player.py
+++ utils/player.py
@@ -1,71 +1,48 @@
 from card import Card
-
 import random
 
+
 class Player:
-    def __int__(self,card,turn_count,number_of_cards,history,player_name):
-        self.card=Card()
-        self.turn_count=int(turn_count)
-        self.number_of_cards=int(number_of_cards)
-        self.player_name=player_name
-        self.history=Card()
+
+    def __int__(self, card, turn_count, number_of_cards, history, player_name):
+        self.card_list = []
+        self.history_list = []
+        self.card = Card()
+        self.card_list.append(self.card)
+        self.turn_count = int(turn_count)
+        self.number_of_cards = int(number_of_cards)
+        self.player_name = player_name
+        self.history = Card()
+        self.history_list.append(self.history)
 
     def play(self):
-        rand_card= self.card[random.randint(0,len(self.card))]
-        self.history.append(rand_card)
-        self.card.remove(rand_card)
+        rand_card = self.card_list[random.randint(0, len(self.card_list))]
+        self.history_list.append(rand_card)
+        self.card_list.remove(rand_card)
         print(f'{self.player_name} {self.turn_count} played:{rand_card}{Card.value}')
 
         return rand_card
 
+
 class Deck(Player):
-    def __del__(self, cards):
-        self.cards=Card[[]]
+
+    def __init__(self):
+        self.card_symb = ['♥', '♦', '♣', '♠']
+        self.card_num = ['A', '2', '3', '4 ', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        self.cards = []
+        self.sample_card = Card()
 
     def fill_deck(self):
-        for i in Card.icon.len():
-            for j in Card.value.len():
-                self.cards.append([Card.icon[i],Card.value[j],'red'])
-                self.cards.append([Card.icon[i],Card.value[j],'black'])
+        for i in self.card_symb:
+            for j in self.card_num:
+                self.cards.append([i, j])
 
     def shuffle(self):
         random.shuffle(self.cards)
 
-    def distribute(self):
-        no_of_players=Player[]
-        j=0
+    def distribute(self, no_of_players):
+        self.no_of_players = no_of_players
+        j = 0
         for i in no_of_players:
             no_of_players[i].card.append(self.cards[i][j])
-            j+=1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            j += 1
