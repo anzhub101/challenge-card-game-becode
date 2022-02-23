e069d39af4f2110541ac9bb5df5f97814c317a13
anzhub101 <anzeemug@gmail.com>
2022-02-23 01:50:41 +0100

    update on work

--- utils/player.py
+++ utils/player.py
@@ -2,35 +2,17 @@ from card import Card
 import random
 
 
-class Player:
+class Deck:
 
-    def __int__(self, card, turn_count, number_of_cards, history, player_name):
-        self.card_list = []
-        self.history_list = []
-        self.card = Card()
-        self.card_list.append(self.card)
-        self.turn_count = int(turn_count)
-        self.number_of_cards = int(number_of_cards)
-        self.player_name = player_name
-        self.history = Card()
-        self.history_list.append(self.history)
-
-    def play(self):
-        rand_card = self.card_list[random.randint(0, len(self.card_list))]
-        self.history_list.append(rand_card)
-        self.card_list.remove(rand_card)
-        print(f'{self.player_name} {self.turn_count} played:{rand_card}{Card.value}')
-
-        return rand_card
-
-
-class Deck(Player):
-
-    def __init__(self):
+    def __init__(self, no_of_players, players_name):
         self.card_symb = ['♥', '♦', '♣', '♠']
         self.card_num = ['A', '2', '3', '4 ', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         self.cards = []
-        self.sample_card = Card()
+        self.player_hands = []
+        self.no_of_players = no_of_players
+        self.player_list = []
+        self.players_name = players_name
+        self.hands=[]
 
     def fill_deck(self):
         for i in self.card_symb:
@@ -40,9 +22,51 @@ class Deck(Player):
     def shuffle(self):
         random.shuffle(self.cards)
 
-    def distribute(self, no_of_players):
-        self.no_of_players = no_of_players
-        j = 0
-        for i in no_of_players:
-            no_of_players[i].card.append(self.cards[i][j])
-            j += 1
+    def distribute(self):
+        if self.no_of_players/2==0:
+            cards_per_person=52/self.no_of_players
+            j = 0
+            for i in range(0,51):
+                for i in range(0, cards_per_person - 1):
+                    self.player_hands.append(self.cards[i][j])
+                    j += 1
+                self.hands.append(self.player_hands)
+        else:
+            remainder=52%self.no_of_players
+            print(f'Since you are an odd no. of players, the first {remainder} of you will get an extra card')
+            cards_per_person = 52 / self.no_of_players
+            cards_per_person=int(cards_per_person)
+            j = 0
+            for i in range(0, 51-remainder):
+                for i in range(0, cards_per_person - 1):
+                    self.player_hands.append(self.cards[i][j])
+                    j += 1
+                self.hands.append(self.player_hands)
+            j = 0
+            for i in range(51-remainder, 51):
+                self.player_hands[j].append(self.cards[i][i])
+                j+=1
+
+
+    def player_profile(self):
+        for i in self.no_of_players:
+            input(self.players_name)
+            self.player_list.append(self.players_name)
+
+class Player(Deck):
+
+    def __int__(self, card, turn_count, number_of_cards, history, player_name):
+
+        self.card = hands
+        self.turn_count = int(turn_count)
+        self.number_of_cards = int(number_of_cards)
+        self.player_name = Deck.players_name
+        self.history = []
+
+    def play(self):
+        rand_card = self.card[random.randint(0, len(self.card))]
+        self.history.append(rand_card)
+        self.card.remove(rand_card)
+        print(f'{self.player_name} {self.turn_count} played:{rand_card}')
+
+        return rand_card
